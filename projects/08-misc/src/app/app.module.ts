98e5df30d32d4b7f8edc0ae6942e7cf8e7de0235
jfzuluag <jfzuluag@bancolombia.com.co>
2020-09-22 23:21:10 -0500

    manejo de rutas, y otras funcionalidades de interes

--- projects/08-misc/src/app/app.module.ts
+++ projects/08-misc/src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { APP_ROUTING } from './app.routes';
+
+
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
 import { ClassesComponent } from './components/classes/classes.component';
+import { ResaltadoDirective } from './directives/resaltado.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserComponent } from './components/user/user.component';
+import { EditUserComponent } from './components/user/edit-user.component';
+import { DetailUserComponent } from './components/user/detail-user.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStyleComponent,
     CssComponent,
-    ClassesComponent
-  ],
+    ClassesComponent,
+    ResaltadoDirective,
+    NgSwitchComponent,
+    HomeComponent,
+    UserComponent,
+    EditUserComponent,
+    DetailUserComponent,
+    NavbarComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
